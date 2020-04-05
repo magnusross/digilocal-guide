2063007b776f16662a65e8e01c52fa3fbc11e53d
magnusross <mr16064@bristol.ac.uk>
2020-04-05 21:23:10 +0100

    initial

---
+++ predictors.py
@@ -0,0 +1,51 @@
+import pandas 
+from sklearn import tree 
+
+
+data_train = pandas.read_pickle('data_train.pkl')
+ans_train = pandas.read_pickle('ans_train.pkl')
+
+def my_tree(animal):
+    # if animal has no backbone it is not a predator
+    if animal['backbone'] == 0:
+        return 0 
+
+    else:
+        # if animal has no teeth it is not a predator
+        if animal['toothed'] == 0:
+            return 0
+        # if animal has teeth and backbone it is a predator
+        else:
+            return 1 
+
+bear_row = data_train.iloc[3]
+print(my_tree(bear_row))
+
+def get_predictions(data):
+
+    predictions = []
+    for _, animal in data.iterrows():
+        prediction = my_tree(animal)
+        predictions.append(prediction)
+
+    return predictions
+
+
+def test_predictions(predictions, answers):
+    correct = 0
+    for pred, ans in zip(predictions, answers):
+        if pred == ans:
+            correct += 1
+    print('%s/%s correct!'%(correct, len(answers))) 
+
+
+
+predictions = get_predictions(data_train)
+test_predictions(predictions, ans_train)
+
+auto_tree = tree.DecisionTreeClassifier()
+auto_tree = auto_tree.fit(data_train.drop(columns='animal name'), ans_train)
+
+auto_preds = auto_tree.predict(data_train.drop(columns='animal name'))
+
+test_predictions(auto_preds, ans_train) 
